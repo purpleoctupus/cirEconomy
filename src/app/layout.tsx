d57cdffbfd95390de92ca055e0b7aa4f519feae6
smontoya2 <smontoya@makesystems.com.co>
2025-04-28 21:08:48 -0500

    se usa el footer en las paginas pendientes por contenido

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import AuthProvider from "./infraestructure/providers/SessionProvider";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <AuthProvider>
           <Navbar />
           {children}
+          <Footer />
         </AuthProvider>
       </body>
     </html>
